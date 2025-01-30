ae05072ae831dc8fadf52b9822fabe3029c90277
vidhiagrawa <vidhiagrawal2205@gmail.com>
2025-01-29 22:54:45 -0800

    adding somrthing

--- react_pwd_generator/src/App.jsx
+++ react_pwd_generator/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [Passtatus, setPasstatus] = useState(false)
+  const [ value , setValue ] = useState(null)
+  useEffect( () => {
+    console.log(value)
+  } , [value] )
   return (
     <>
       <div className = "App">
-        <input className = "inpt" type = {( Passtatus ) ? 'text' : 'password' }  />
-        <button className = "butn" onClick={() => setPasstatus(!Passtatus)}>
+        <input className = "inpt" value={value} onChange={ (e) => setValue(e.target.value) } type = {( Passtatus ) ? 'text' : 'password' }/>
+        <button className = "butn" onClick={() => {
+          ( value == null ) ? alert("Input is null") : null
+            setPasstatus(!Passtatus)
+          }}>
           {(Passtatus) ? 'Hide' : 'Show'}
         </button>
       </div>
