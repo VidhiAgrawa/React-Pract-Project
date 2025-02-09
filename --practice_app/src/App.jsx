b3b5adac732b4d73137b79112f854e831b749460
vidhiagrawa <vidhiagrawal2205@gmail.com>
2025-02-08 19:48:50 -0800

    adding

--- --practice_app/src/App.jsx
+++ --practice_app/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  let count = 0
+  useEffect(() => {
+    console.log(count)
+    count++
+  }, [])
   const [change, setChange] = useState("red")
-
+  
   return (
     <>
       <div className='colors'>
         <input type="color" className='inpt' onChange={(e) => setChange(e.target.value)} />
-        <div className='colourBox' style = {{background: change}} ></div>
+        <button onClick={() => console.log(count)}>count</button>
+        <div className='colourBox' style = {{background: change}} >{count}</div>
       </div>
     </>
   )
